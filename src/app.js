a09184385c13d3726477235034442e6908012c17
raachelkk <116052393+raachelkk@users.noreply.github.com>
2022-12-03 12:05:00 -0800

    Added real data for date, simplified JS HTML replacements

--- src/app.js
+++ src/app.js
@@ -1,19 +1,46 @@
-function displayTemperature(response) {
-  console.log(response.data.temperature.current);
-  let temperatureElement = document.querySelector("#current-temp");
-  let cityElement = document.querySelector("#city");
-  let conditionElement = document.querySelector("#condition");
-  let humidityElement = document.querySelector("#humidity");
-  let windEelement = document.querySelector("#wind");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.city;
-  conditionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = response.data.temperature.humidity;
-  windEelement.innerHTML = Math.round(response.data.wind.speed);
+  return `${day} ${hours}:${minutes}`;
 }
 
+function displayTemperature(response) {
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#condition").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
+  );
+}
+
+let city = "London";
 let apiKey = "82e2a506b3bf38ccef35bc49eo4bd3at";
-let url = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(url).then(displayTemperature);
