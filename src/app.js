13083e9752d405fd040aeb0a566f7d24aa0f8848
raachelkk <116052393+raachelkk@users.noreply.github.com>
2022-12-03 11:37:35 -0800

    Added weather API and implemented real data for all but icon and time

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windEelement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windEelement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "82e2a506b3bf38ccef35bc49eo4bd3at";
+let url = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+
+axios.get(url).then(displayTemperature);
